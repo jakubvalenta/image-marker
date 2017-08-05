61c4e0bad3863b9464b0fc671cd3220dba1d19bf
Jakub Valenta <jakub@jakubvalenta.cz>
2017-08-06 01:36:32 +0200

    Add support for box with defined ratio and padding

--- image_marker/image_marker.py
+++ image_marker/image_marker.py
@@ -24,6 +24,7 @@ TColor = Tuple[int, int, int, int]
 TMarks = Dict[TPath, Rectangle]
 
 COLOR_RED = (255, 0, 0, 255)
+COLOR_BLUE = (0, 255, 0, 255)
 
 
 class ImageMarker(pyglet.window.Window):
@@ -36,12 +37,16 @@ class ImageMarker(pyglet.window.Window):
     def __init__(self,
                  images: List[TPath],
                  marks: TMarks,
-                 callback: Callable[[TPath, Rectangle], None],
+                 callback: Callable[[TPath, Rectangle, Rectangle], None],
+                 box_ratio: float,
+                 box_pad_perc_w: float,
                  *args,
                  **kwargs):
         self.images = images
         self.marks = marks
         self.callback = callback
+        self.box_ratio = box_ratio
+        self.box_pad_perc_w = box_pad_perc_w
         super().__init__(*args, **kwargs)
         self.load_image()
 
@@ -49,18 +54,24 @@ class ImageMarker(pyglet.window.Window):
     def current_image(self) -> TPath:
         return self.images[self.current_image_idx]
 
-    def rect_to_mark(self, rect: Rectangle) -> Rectangle:
+    @staticmethod
+    def normalize_rect(rect: Rectangle) -> Rectangle:
         vertex_1 = rect.x, rect.y
         vertex_2 = rect.x + rect.w, rect.y + rect.h
-        left = min(vertex_1[0], vertex_2[0])
-        top = max(vertex_1[1], vertex_2[1])
-        width = abs(rect.w)
-        height = abs(rect.h)
+        x = min(vertex_1[0], vertex_2[0])
+        y = min(vertex_1[1], vertex_2[1])
+        w = abs(rect.w)
+        h = abs(rect.h)
+        return Rectangle(x, y, w, h)
+
+    def rect_to_mark(self, rect: Rectangle) -> Rectangle:
+        normalized = self.normalize_rect(rect)
+        position_x, position_y = self.gl_sprite.position
         coord_original = (
-            left - self.gl_sprite.position[0],
-            self.height - top - self.gl_sprite.position[1],
-            width,
-            height,
+            normalized.x - position_x,
+            self.height - normalized.y - normalized.h - position_y,
+            normalized.w,
+            normalized.h,
         )
         coord_scaled = map(lambda x: round(x / self.gl_sprite.scale),
                            coord_original)
@@ -75,6 +86,23 @@ class ImageMarker(pyglet.window.Window):
             w=mark.w * scale,
             h=mark.h * scale
         )
+
+    def calc_box(self,
+                 rect: Rectangle,
+                 ratio: float,
+                 pad_perc_w: float=0.2) -> Rectangle:
+        normalized = self.normalize_rect(rect)
+        position_x, position_y = self.gl_sprite.position
+        sprite_w, sprite_h = self.gl_sprite.width, self.gl_sprite.height
+        contain_w, contain_h = self.contain(normalized.w, normalized.h, ratio)
+        pad_px = contain_w * pad_perc_w * 2
+        w = min(contain_w + pad_px, self.gl_sprite.width)
+        h = min(contain_h + pad_px, self.gl_sprite.height)
+        x = normalized.x + normalized.w / 2 - w / 2
+        y = normalized.y + normalized.h / 2 - h / 2
+        x_limited = min(max(x, position_x), position_x + sprite_w - w)
+        y_limited = min(max(y, position_y), position_y + sprite_h - h)
+        return Rectangle(x_limited, y_limited, w, h)
 
     def reload_rect(self):
         self.gl_rect = None
@@ -122,6 +150,17 @@ class ImageMarker(pyglet.window.Window):
             position_y = 0
         return ratio, position_x, position_y
 
+    @staticmethod
+    def contain(w, h, container_ratio):
+        ratio = w / h
+        if ratio > container_ratio:
+            container_w = w
+            container_h = w / container_ratio
+        else:
+            container_h = h
+            container_w = h * container_ratio
+        return container_w, container_h
+
     def on_draw(self):
         self.clear()
         ratio, position_x, position_y = self.fit(
@@ -133,15 +172,20 @@ class ImageMarker(pyglet.window.Window):
         self.gl_sprite.scale = ratio
         self.gl_sprite.position = (position_x, position_y)
         self.gl_sprite.draw()
-        if self.gl_rect:
-            self.draw_rect(self.gl_rect)
-        elif self.current_mark:
+        if not self.gl_rect and self.current_mark:
             self.gl_rect = self.mark_to_rect(self.current_mark)
+        if self.gl_rect and self.gl_rect.w and self.gl_rect.h:
             self.draw_rect(self.gl_rect)
+            if self.box_ratio:
+                self.gl_box = self.calc_box(self.gl_rect,
+                                            self.box_ratio,
+                                            self.box_pad_perc_w)
+                self.draw_rect(self.gl_box, COLOR_BLUE)
 
     def on_mouse_press(self, x, y, button, modifiers):
         if button == mouse.LEFT:
             self.gl_rect = Rectangle(x, y, 0, 0)
+            self.gl_box = None
 
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
         if buttons & mouse.LEFT:
@@ -151,8 +195,12 @@ class ImageMarker(pyglet.window.Window):
     def on_mouse_release(self, x, y, button, modifiers):
         if button == mouse.LEFT:
             self.current_mark = self.rect_to_mark(self.gl_rect)
+            if self.gl_box:
+                box = self.rect_to_mark(self.gl_box)
+            else:
+                box = self.current_mark
             self.marks[self.current_image] = self.current_mark
-            self.callback(self.current_image, self.current_mark)
+            self.callback(self.current_image, self.current_mark, box)
 
     def on_key_press(self, symbol, modifiers):
         if symbol in (key.ENTER, key.SPACE, key.RIGHT, key.DOWN):
@@ -193,24 +241,27 @@ def write_marks(marks: Iterable[TMarks], path: TPath):
 
 @click.command()
 @click.argument('images_dir')
+@click.option('--output', '-o', 'output_path', required=True)
 @click.option('--marks', '-m', 'marks_path')
-@click.option('--output', '-o', 'output_path')
-def cli(images_dir, output_path, marks_path):
+@click.option('--box-ratio', '-br', type=float, default=0)
+@click.option('--box-pad-perc-w', '-bp', type=float, default=0.2)
+def cli(images_dir, output_path, marks_path, box_ratio, box_pad_perc_w):
     paths = read_paths(images_dir)
     marks = read_marks(marks_path)
 
-    if output_path:
-        out = {}
+    out = {}
 
-        def callback(path, mark):
-            out[path] = mark
-            write_marks(out, output_path)
+    def callback(path, mark, box):
+        out[path] = mark
+        write_marks(out, output_path)
+        print(path, box.x, box.y, box.w, box.h)
 
-    else:
-        def callback(path, mark):
-            print(path, *mark)
-
-    ImageMarker(paths, marks, callback, resizable=True)
+    ImageMarker(paths,
+                marks,
+                callback,
+                box_ratio,
+                box_pad_perc_w,
+                resizable=True)
     pyglet.app.run()
 
 
